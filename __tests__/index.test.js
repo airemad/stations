652a5c6307179383e38fb035eef259015594e98e
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-04-29 18:05:57 +0200

    feat: added proper tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,34 @@
-describe('Sample test', () => {
-  it('Should pass', () => {
-    expect(true).toBe(true)
+const { getAll } = require('../src')
+const stations = require('../src/stations.json')
+const schema = require('../src/schema.json')
+
+const Ajv = require('ajv')
+const addFormats = require('ajv-formats')
+
+const ajv = new Ajv()
+addFormats(ajv)
+
+describe('Stations dataset', () => {
+  it('Should follow the json schema', () => {
+    const validate = ajv.compile(schema)
+    const valid = validate(stations)
+    expect(valid).toEqual(true)
+    expect(validate.errors).toEqual(null)
+  })
+
+  it('Should have unique stations', () => {
+    const ids = new Set(stations.map(({ id }) => id))
+    expect(ids.size).toBe(stations.length)
+  })
+
+  it('Should have unique uuids', () => {
+    const uuids = new Set(stations.map(({ uuid }) => uuid))
+    expect(uuids.size).toBe(stations.length)
+  })
+})
+
+describe('Stations methods', () => {
+  it('Should return all the stations', () => {
+    expect(getAll()).toStrictEqual(stations)
   })
 })
